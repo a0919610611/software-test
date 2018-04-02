1e4e0059bf5bca2db9b0b178c1c35efb6102f839
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-02 02:28:28 -0700

    add interface and enum type of function GetTraingleTypewhich the function needed to be tested in triangle problem

--- hw1/Triangle/triangle.h
+++ hw1/Triangle/triangle.h
@@ -1,3 +1,15 @@
 #ifndef TRIANGLE
 #define TRIANGLE
+
+enum TriangleType { Equilateral,
+    Scalene,
+    Isosceles,
+    NotTriangle };
+
+TriangleType GetTriangleType(int a, int b, int c)
+{
+
+    return NotTriangle;
+};
+
 #endif
