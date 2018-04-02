c13be48af04789774344642a757dca34023d7c32
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-02 02:59:06 -0700

    add test structure , ready to add test case

---
+++ hw1/Triangle/triangle_unittest.cpp
@@ -0,0 +1,20 @@
+#include "triangle.h"
+#include <gtest/gtest.h>
+
+TEST(Boundary_Value, weak)
+{
+}
+TEST(Boundary_Value, strong)
+{
+}
+TEST(Boundary_Value, robust)
+{
+}
+TEST(Boundary_Value, normal)
+{
+}
+
+int main(int argc, char** argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
