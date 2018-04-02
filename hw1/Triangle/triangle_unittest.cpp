39317cbfd0bbc37cedfea6064dc312bd0383e74e
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-02 06:44:14 -0700

    add normal bounary test case , start write feature code

--- hw1/Triangle/triangle_unittest.cpp
+++ hw1/Triangle/triangle_unittest.cpp
@@ -3,13 +3,26 @@
 
 TEST(Boundary_Value, normal)
 {
-    EXPECT_EQ(Equilateral,GetTriangleType(1,1,1));
-}
-TEST(Boundary_Value, robust)
-{
+    // From slides
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 100, 1));
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 100, 2));
+    EXPECT_EQ(Equilateral, GetTriangleType(100, 100, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 100, 199));
+    EXPECT_EQ(NotTriangle, GetTriangleType(100, 100, 200));
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 1, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 2, 100));
+    EXPECT_EQ(Equilateral, GetTriangleType(100, 100, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(100, 199, 100));
+    EXPECT_EQ(NotTriangle, GetTriangleType(100, 200, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(1, 100, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(2, 100, 100));
+    EXPECT_EQ(Equilateral, GetTriangleType(100, 100, 100));
+    EXPECT_EQ(Isosceles, GetTriangleType(199, 100, 100));
+    EXPECT_EQ(NotTriangle, GetTriangleType(200, 100, 100));
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
