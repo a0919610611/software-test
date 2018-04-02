3330406bdffe4835c6c969869a025e5290cfe7d8
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-02 06:56:19 -0700

    [triangle] add Scalene & Isosceles check but failed some test

--- hw1/Triangle/triangle.cpp
+++ hw1/Triangle/triangle.cpp
@@ -7,5 +7,10 @@ TriangleType GetTriangleType(int a, int b, int c)
     if (a == b && b == c) {
         return Equilateral;
     }
+    if (a != b && a != c && b != c) {
+        return Scalene;
+    } else {
+        return Isosceles;
+    }
     return NotTriangle;
 };
