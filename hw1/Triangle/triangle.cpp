c183f364b161dbd6eaccb6a2c99a720527cf4906
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-02 07:24:04 -0700

    [triangle] add a+b>c checking and pass all test

--- hw1/Triangle/triangle.cpp
+++ hw1/Triangle/triangle.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 TriangleType GetTriangleType(int a, int b, int c)
 {
-    if (a == b && b == c) {
+    // two edges' sum greter than 3'rd edge
+    if (not(a + b > c and b + c > a and a + c > b)) {
+        return NotTriangle;
+    }
+    if (a == b and b == c) {
         return Equilateral;
     }
-    if (a != b && a != c && b != c) {
+    if (a != b and a != c and b != c) {
         return Scalene;
     } else {
         return Isosceles;
