2f1f0bee38a1202c87a92cba790400ec1d3033c7
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-05 01:55:32 -0700

    [triangle] add worst case boundary value test, still pass the test

--- hw1/Triangle/triangle_unittest.cpp
+++ hw1/Triangle/triangle_unittest.cpp
@@ -1,9 +1,8 @@
 #include "triangle.h"
 #include <gtest/gtest.h>
 
-TEST(Boundary_Value, normal)
+TEST(Boundary_Value, normal_single_fault)
 {
-    // From slides
     EXPECT_EQ(Isosceles, GetTriangleType(100, 100, 1));
     EXPECT_EQ(Isosceles, GetTriangleType(100, 100, 2));
     EXPECT_EQ(Equilateral, GetTriangleType(100, 100, 100));
@@ -19,6 +18,19 @@ TEST(Boundary_Value, normal)
     EXPECT_EQ(Equilateral, GetTriangleType(100, 100, 100));
     EXPECT_EQ(Isosceles, GetTriangleType(199, 100, 100));
     EXPECT_EQ(NotTriangle, GetTriangleType(200, 100, 100));
+}
+
+TEST(Boundary_Value, normal_multiple_fault_selected)
+{
+    EXPECT_EQ(Equilateral, GetTriangleType(1, 1, 1));
+    EXPECT_EQ(NotTriangle, GetTriangleType(1, 1, 2));
+    EXPECT_EQ(NotTriangle, GetTriangleType(1, 1, 100));
+    EXPECT_EQ(NotTriangle, GetTriangleType(1, 1, 199));
+    EXPECT_EQ(NotTriangle, GetTriangleType(1, 1, 200));
+    EXPECT_EQ(Isosceles,GetTriangleType(1,2,2));
+    EXPECT_EQ(Isosceles,GetTriangleType(1,100,100));
+    EXPECT_EQ(Isosceles,GetTriangleType(1,199,199));
+    EXPECT_EQ(Isosceles,GetTriangleType(1,200,200));
 }
 
 int main(int argc, char** argv)
