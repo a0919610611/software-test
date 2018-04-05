2d555e298cbaa8c766d7a91883982a8a5c26a9f9
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-05 12:20:40 -0700

    [nextdate] add decision table based test cases, still pass the test

--- hw1/NextDate/nextdate_unittest.cpp
+++ hw1/NextDate/nextdate_unittest.cpp
@@ -124,7 +124,6 @@ TEST(Equivalence_Class, strong_robust)
     EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(1912, -1, -1)));
     EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(1811, 6, -1)));
     EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(1811, 13, 15)));
-    EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(1811, 13, -1)));
     EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(1811, -1, -1)));
 }
 
@@ -133,10 +132,23 @@ TEST(Equivalence_Class, strong_normal)
     EXPECT_EQ(Date(1912, 6, 16), NextDate(Date(1912, 6, 15)));
 }
 
-/*
-TEST(Decision_Table, decisions){}
+TEST(Decision_Table, decisions)
+{
+    EXPECT_EQ(Date(2001, 4, 16), NextDate(Date(2001, 4, 15)));
+    EXPECT_EQ(Date(2001, 5, 1), NextDate(Date(2001, 4, 30)));
+    EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(2001, 4, 31)));
+    EXPECT_EQ(Date(2001, 1, 16), NextDate(Date(2001, 1, 15)));
+    EXPECT_EQ(Date(2001, 2, 1), NextDate(Date(2001, 1, 31)));
+    EXPECT_EQ(Date(2001, 12, 16), NextDate(Date(2001, 12, 15)));
+    EXPECT_EQ(Date(2002, 1, 1), NextDate(Date(2001, 12, 31)));
+    EXPECT_EQ(Date(2001, 2, 16), NextDate(Date(2001, 2, 15)));
+    EXPECT_EQ(Date(2004, 2, 29), NextDate(Date(2004, 2, 28)));
+    EXPECT_EQ(Date(2001, 3, 1), NextDate(Date(2001, 2, 28)));
+    EXPECT_EQ(Date(2004, 3, 1), NextDate(Date(2004, 2, 29)));
+    EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(2001, 2, 29)));
+    EXPECT_EQ(Date(-1, -1, -1), NextDate(Date(2001, 2, 30)));
+}
 
-*/
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
