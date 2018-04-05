08d2c38bf0cd3c415bddefcb7fe34198e3dfb59e
Yu-Fu, Fu <a0919610611@gmail.com>
2018-04-05 02:01:36 -0700

    [triangle] pass robust bounary value test

--- hw1/Triangle/triangle.cpp
+++ hw1/Triangle/triangle.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 TriangleType GetTriangleType(int a, int b, int c)
 {
+    // check the range of edge value
+
+    if(a>200 or a<1 or b>200 or b<1 or c>200 or c<1){
+        return ValueRangeError;
+    }
+
     // two edges' sum greter than 3'rd edge
     if (not(a + b > c and b + c > a and a + c > b)) {
         return NotTriangle;
