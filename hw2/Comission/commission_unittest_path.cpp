45bc48dd71da59ae5e5e6346cf965602af63053b
a0919610611 <a0919610611@gmail.com>
2018-05-14 23:49:55 +0800

    [commission] finish C0,C1,C2,MCDC

--- hw2/Comission/commission_unittest_path.cpp
+++ hw2/Comission/commission_unittest_path.cpp
@@ -1,6 +1,41 @@
 #include "commission.h"
 #include <gtest/gtest.h>
-  
+
+TEST(Path_Testing,C0)
+{
+  EXPECT_DOUBLE_EQ(640.0, CalCommission(35, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(0, 40, 45));
+  EXPECT_DOUBLE_EQ(160.75, CalCommission(30, 1, 1));
+  EXPECT_DOUBLE_EQ(95.5, CalCommission(20, 1, 1));
+}
+TEST(Path_Testing,C1)
+{
+  EXPECT_DOUBLE_EQ(640.0, CalCommission(35, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(0, 40, 45));
+  EXPECT_DOUBLE_EQ(160.75, CalCommission(30, 1, 1));
+  EXPECT_DOUBLE_EQ(95.5, CalCommission(20, 1, 1));
+}
+TEST(Path_Testing,C2)
+{
+  EXPECT_DOUBLE_EQ(640.0, CalCommission(35, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(0, 40, 45));
+  EXPECT_DOUBLE_EQ(160.75, CalCommission(30, 1, 1));
+  EXPECT_DOUBLE_EQ(95.5, CalCommission(20, 1, 1));
+}
+TEST(Path_Testing,MCDC)
+{
+  EXPECT_DOUBLE_EQ(640.0, CalCommission(35, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(0, 40, 45));
+  EXPECT_DOUBLE_EQ(160.75, CalCommission(30, 1, 1));
+  EXPECT_DOUBLE_EQ(95.5, CalCommission(20, 1, 1));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(0, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(71, 40, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(35, 0, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(35, 81, 45));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(35, 40, 0));
+  EXPECT_DOUBLE_EQ(-1.0, CalCommission(35, 40, 91));
+}
+
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
